cdb0d582168a49aa057501e751e774653ab3e15d
MUKILTEO\1725558 <1725558@mukilteo.wednet.edu>
2025-10-03 09:35:21 -0700

    Added enemy pathfinding

---
+++ 2D Template/Assets/Scripts/Jayden T/Enemy AI Script.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+public class EnemyAIScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float moveSpeed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int curentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+        Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+                        curentWaypoint = 0;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
