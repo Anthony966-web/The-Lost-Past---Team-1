ebe66ca29600c6469b91b87461ef4f0f01a258c3
MUKILTEO\1725608 <1725608>
2025-10-09 08:52:16 -0700

    Goblin is NOT a ghost

--- 2D Template/Assets/Scripts/Damage.cs
+++ 2D Template/Assets/Scripts/Damage.cs
@@ -15,7 +15,7 @@ public class Damage : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
